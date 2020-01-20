f7d9d03f2b6eaa374294b63956646bb621467fb3
Kazuki Fujiya <k.fujiya.dev@gmail.com>
2020-01-20 22:44:27 +0900

    unityプロジェクトデータ

---
+++ Assets/Editor/TestRunnerTestTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assert = UnityEngine.Assertions.Assert;
+using NUnit.Framework;
+
+public class TestRunnerTestTest
+{
+    [Test]
+    public void GetTextTest()
+    {
+        //TestRunnerTest script = new TestRunnerTest();
+        string a = TestRunnerTest.GetText(1);
+
+        Assert.AreEqual(a, "id:1");
+    }
+
+}
---
+++ Assets/Scripts/TestRunnerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRunnerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static string GetText(int id)
+    {
+        //Debug.Log(string.Format("id:{0}" + id));
+        return string.Format("id:{0}", id);
+    }
+}
